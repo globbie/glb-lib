33059acef8cec7821bdea09f8fe4c7ef762a9ed1
Ilia K <ki.stfu@gmail.com>
2017-11-30 02:25:02 +0300

    Filter __seq__ tag

--- tools/json2gsl.py
+++ tools/json2gsl.py
@@ -23,6 +23,8 @@ def putc(ch):
 def process_task(in_change = False):
     set_tag = "__set__"
     set_state = -1
+    seq_tag = "__seq__"
+    seq_state = -1
 
     in_field = False
     in_tag = False
@@ -54,13 +56,13 @@ def process_task(in_change = False):
             assert in_terminal_value == False
         elif ch == '"':
             if in_field and not in_tag and not in_value:
-                set_state = 0
+                set_state = 0; seq_state = 0
                 # in_field == True
                 in_tag = True
                 # in_value == False
                 assert in_terminal_value == False
             elif in_field and in_tag:
-                if set_state != len(set_tag): putc(' ')
+                if set_state != len(set_tag) and seq_state != len(seq_tag): putc(' ')
                 # in_field == True
                 in_tag = False
                 assert in_value == False
@@ -95,13 +97,29 @@ def process_task(in_change = False):
             return
         else:
             if in_tag:
-                if set_state != -1:
-                    if set_state < len(set_tag) and ch == set_tag[set_state]:
-                        set_state += 1
-                    else:
+                if set_state != -1 or seq_state != -1:
+                    pending = None
+                    print_pending = True
+                    if set_state != -1:
+                        if set_state < len(set_tag) and ch == set_tag[set_state]:
+                            pending = None
+                            print_pending = False
+                            set_state += 1
+                        else:
+                            pending = set_tag[:set_state]
+                            set_state = -1
+                    if seq_state != -1:
+                        if seq_state < len(seq_tag) and ch == seq_tag[seq_state]:
+                            pending = None
+                            print_pending = False
+                            seq_state += 1
+                        else:
+                            pending = seq_tag[:seq_state]
+                            seq_state = -1
+                    if print_pending and pending != None:
                         putc('{' if not in_change else '(')
-                        putc(set_tag[:set_state]); putc(ch)
-                        set_state = -1
+                        for ch_ in pending: putc(ch_)
+                        putc(ch) # priint
                 else:
                     putc(ch)
             elif in_terminal_value:
