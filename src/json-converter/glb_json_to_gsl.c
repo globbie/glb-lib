3bc4ae84e5becbdf25e2acc80b193442dca46837
Ilia K <ki.stfu@gmail.com>
2017-12-12 00:00:58 +0300

    json-converter: Handle return codes (possible errors) in glb_json_to_gsl(); Do not use assert() for input data

--- src/json-converter/glb_json_to_gsl.c
+++ src/json-converter/glb_json_to_gsl.c
@@ -15,6 +15,7 @@ process_task_array(const char **rec, const char *rec_end,
                    bool in_change, const char *tag, size_t tag_size,
                    struct glbOutput *output)
 {
+    int err = glb_OK;
     bool in_place = tag_size == 0;
 
     const char *c = *rec;
@@ -27,28 +28,37 @@ process_task_array(const char **rec, const char *rec_end,
             break;
         case '[':
             if (!in_place) {
-                output->putc(output, '[');
-                output->write(output, tag, tag_size);
-                output->putc(output, ' ');
+                err = output->putc(output, '[');
+                if (err) goto out;
+                err = output->write(output, tag, tag_size);
+                if (err) goto out;
+                err = output->putc(output, ' ');
+                if (err) goto out;
             }
             c++;
-            process_task(&c, rec_end, in_change, NULL, 0, output);
+            err = process_task(&c, rec_end, in_change, NULL, 0, output);
+            if (err) goto out;
             break;
         case ',':
-            output->putc(output, ' ');
+            err = output->putc(output, ' ');
+            if (err) goto out;
             c++;
-            process_task(&c, rec_end, in_change, NULL, 0, output);
+            err = process_task(&c, rec_end, in_change, NULL, 0, output);
+            if (err) goto out;
             break;
         case ']':
             if (!in_place)
-                output->putc(output, ']');
+                err = output->putc(output, ']');
+            goto out;
+        default:
+            err = glb_FORMAT;
             goto out;
         }
     }
 
 out:
     *rec = c;
-    return 0;
+    return err;
 }
 
 static int
@@ -56,6 +66,7 @@ process_task(const char **rec, const char *rec_end,
              bool in_change, const char *tag, size_t tag_size,
              struct glbOutput *output)
 {
+    int err = glb_OK;
     bool in_place = tag_size == 0;
 
     bool in_tag = false;
@@ -64,7 +75,7 @@ process_task(const char **rec, const char *rec_end,
 
     const char *c = *rec;
     for (; c < rec_end; c++) {
-        assert(!in_tag || !in_value);
+        if (in_tag && in_value) { err = glb_FORMAT; goto out; }
         if (in_terminal_value) assert(in_value);
 
         switch (*c) {
@@ -72,100 +83,140 @@ process_task(const char **rec, const char *rec_end,
         case '\n':
         case '\r':
         case ' ':
-            assert(!in_tag && "error: tag contains spaces");
-            if (in_terminal_value)
-                output->putc(output, *c);
+            if (in_tag) { err = glb_FORMAT; goto out; }  // error: tag contains spaces
+            if (in_terminal_value) {
+                err = output->putc(output, *c);
+                if (err) goto out;
+            }
             break;
         case '{': {
             if (!in_value) {
                 // Starting opening \{ brace.  Ignore it or print the passed |tag|
                 if (!in_place) {
-                    output->putc(output, !in_change ? '{' : '(');
-                    output->write(output, tag, tag_size);
-                    output->putc(output, ' ');
+                    err = output->putc(output, !in_change ? '{' : '(');
+                    if (err) goto out;
+                    err = output->write(output, tag, tag_size);
+                    if (err) goto out;
+                    err = output->putc(output, ' ');
+                    if (err) goto out;
                 }
                 continue;
             }
-            bool tag_is_set = tag_size == strlen("__set__") && !memcmp(tag, "__set__", tag_size);
-            assert(!tag_is_set || !in_change);
-            process_task(&c, rec_end, !tag_is_set ? in_change : true,
-                         !tag_is_set ? tag : NULL, !tag_is_set ? tag_size : 0,
-                         output);
-            assert(in_tag == false);
-            assert(in_value == true); in_value = false;
-            assert(in_terminal_value == false);
+
+            if (in_tag) { err = glb_FORMAT; goto out; }
+            assert(in_value == true);
+            if (in_terminal_value) { err = glb_FORMAT; goto out; }
+
+            const bool tag_is_set = tag_size == strlen("__set__") && !memcmp(tag, "__set__", tag_size);
+            if (tag_is_set && in_change) { err = glb_FORMAT; goto out; }  // error: duplicated __set__
+            err = process_task(&c, rec_end, !tag_is_set ? in_change : true,
+                              !tag_is_set ? tag : NULL, !tag_is_set ? tag_size : 0,
+                              output);
+            if (err) goto out;
+            // in_tag == false
+            in_value = false;
+            // in_terminal == false
             break;
         }
         case '[': {
-            bool tag_is_seq = tag_size == strlen("__seq__") && !memcmp(tag, "__seq__", tag_size);
-            process_task_array(&c, rec_end, in_change,
-                               !tag_is_seq ? tag : NULL,
-                               !tag_is_seq ? tag_size : 0,
-                               output);
-            assert(in_tag == false);
-            assert(in_value == true); in_value = false;
-            assert(in_terminal_value == false);
+            if (in_tag) { err = glb_FORMAT; goto out; }
+            if (!in_value) { err = glb_FORMAT; goto out; }
+            if (in_terminal_value) { err = glb_FORMAT; goto out; }
+
+            const bool tag_is_seq = tag_size == strlen("__seq__") && !memcmp(tag, "__seq__", tag_size);
+            err = process_task_array(&c, rec_end, in_change,
+                                     !tag_is_seq ? tag : NULL,
+                                     !tag_is_seq ? tag_size : 0,
+                                     output);
+            if (err) goto out;
+            // in_tag == false
+            in_value = false;
+            // in_terminal_value == false
             break;
         }
         case '"':
             if (!in_tag && !in_value) {
+                if (in_terminal_value) { err = glb_FORMAT; goto out; }
+
                 tag = c + 1;
                 tag_size = 0;
                 in_tag = true;
                 // in_value == false
-                assert(in_terminal_value == false);
+                // in_terminal_value == false
             }
             else if (in_tag) {
+                if (in_value) { err = glb_FORMAT; goto out; }
+                if (in_terminal_value) { err = glb_FORMAT; goto out; }
+
                 // |tag| contains the field's tag
                 in_tag = false;
-                assert(in_value == false);
-                assert(in_terminal_value == false);
+                // in_value == false
+                // in_terminal_value == false
             }
             else if (in_value && !in_terminal_value) {
-                output->putc(output, !in_change ? '{' : '(');
-                output->write(output, tag, tag_size);
-                output->putc(output, ' ');
                 assert(in_tag == false);
+
+                err = output->putc(output, !in_change ? '{' : '(');
+                if (err) goto out;
+                err = output->write(output, tag, tag_size);
+                if (err) goto out;
+                err = output->putc(output, ' ');
+                if (err) goto out;
+                // in_tag == false
                 // in_value == true
                 in_terminal_value = true;
             }
             else if (in_value && in_terminal_value) {
-                output->putc(output, !in_change ? '}' : ')');
-                assert(in_tag == false);
+                if (in_tag) { err = glb_FORMAT; goto out; }
+
+                err = output->putc(output, !in_change ? '}' : ')');
+                if (err) goto out;
+                // in_tag == false
                 in_value = false;
                 in_terminal_value = false;
             }
             break;
         case ':':
-            assert(in_tag == false);
-            assert(in_value == false); in_value = true;
-            assert(in_terminal_value == false);
+            if (in_tag) { err = glb_FORMAT; goto out; }
+            if (in_value) { err = glb_FORMAT; goto out; }
+            if (in_terminal_value) { err = glb_FORMAT; goto out; }
+
+            in_value = true;
             break;
         case ',':
-            output->putc(output, ' ');
-            assert(in_tag == false);
-            assert(in_value == false);
-            assert(in_terminal_value == false);
+            if (in_tag) { err = glb_FORMAT; goto out; }
+            if (in_value) { err = glb_FORMAT; goto out; }
+            if (in_terminal_value) { err = glb_FORMAT; goto out; }
+
+            err = output->putc(output, ' ');
+            if (err) goto out;
             break;
         case '}':
+            if (in_tag) { err = glb_FORMAT; goto out; }
+            if (in_value) { err = glb_FORMAT; goto out; }
+            if (in_terminal_value) { err = glb_FORMAT; goto out; }
+
             if (!in_place)
-                output->putc(output, !in_change ? '}' : ')');
-            assert(in_tag == false);
-            assert(in_value == false);
-            assert(in_terminal_value == false);
+                err = output->putc(output, !in_change ? '}' : ')');
             goto out;
         default:
             if (in_tag)
                 tag_size++;
-            else if (in_terminal_value)
-                output->putc(output, *c);
+            else if (in_terminal_value) {
+                err = output->putc(output, *c);
+                if (err) goto out;
+            }
+            else {
+                err = glb_FORMAT;
+                goto out;
+            }
             break;
         }
     }
 
 out:
     *rec = c;
-    return 0;
+    return err;
 }
 
 int
