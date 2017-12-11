5c3302c4c62e6035ed5de7cc1cdac7e11ca787f7
Ilia K <ki.stfu@gmail.com>
2017-12-11 23:21:59 +0300

    tools: Add example for glb_json_to_glb() that called json2gsl + input data files

---
+++ tools/json2gsl.c
@@ -0,0 +1,30 @@
+#include "glb_json_converter.h"
+#include "glb_output.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, const char *argv[])
+{
+    int exit_code = EXIT_FAILURE;
+    struct glbOutput *input = NULL, *output = NULL;
+
+    if (glb_OK != glbOutput_new(&input, 1e6)) goto final;
+    if (glb_OK != input->write_file_content(input, argv[1])) goto final;
+
+    if (glb_OK != glbOutput_new(&output, 1e6)) goto final;
+
+    if (glb_OK != glb_json_to_gsl(input->buf, input->buf_size, NULL, output)) goto final;
+
+    printf("%.*s\n\n", (int)output->buf_size, output->buf);
+
+    exit_code = EXIT_SUCCESS;
+
+final:
+    if (input != NULL)
+        input->del(input);
+    if (output != NULL)
+        output->del(output);
+    return exit_code;
+}
+
